ab2b671229a20300e7de54400eb59792be47d94a
Richard Tae <taerichard@yahoo.com>
2018-04-05 15:06:18 -0700

    admin view 3:06

--- scripts/views/admin-view.js
+++ scripts/views/admin-view.js
@@ -5,30 +5,51 @@ var app = app || {};
 (function (module) {
   const adminView = {};
 
-  adminView.initAdminPage = function (ctx, next) {
+  adminView.initAdminPage = function (ctx) {
     $('.nav-menu').slideUp(350);
     $('.admin-view').show();
-
     $('#admin-form').on('submit', function(event) {
       event.preventDefault();
-      let token = event.target.passphrase.value;
-
-
-      //Adds userTokens to localstorage.  
-      /*$.get(`${ENV.apiURL})/admin`, {token})
+      const submittedName = $('#admin-form input')[0].value;
+      console.log(submittedName);
+      localStorage.removeItem('stored');
+      localStorage.setItem('stored',submittedName);
+      console.log(localStorage.stored);
+      console.log(ENV);
+      console.log(ENV.apiUrl);
+      //Adds usersubmittedNames to localstorage.
+      
+      $.ajax({url:`${ENV.apiUrl}/admin`,
+        type:'GET',
+        headers:{token:submittedName},
+      })
+        .then(function(respondToken){
+          if(respondToken===submittedName)
+          {
+            console.log(respondToken);
+            console.log(submittedName);
+            console.log("access granted");
+          }
+          else{
+            console.log(respondToken);
+            console.log(submittedName);
+            console.log('rejected.')
+          })
         .then(res =>{
-          localStorage.token = true;
-          page('/');
+          if(res===localStorage.stored)
+          {console.log('Access Granted')
+            localStorage.submittedName = true;
+            page('/');
+          }
         })
-        .catch(() =>page('/'));*/
-    })
-  };
-
-    adminView.verify = function(ctx, next) {
-      if(!localStorage.token) $('.admin').addClass('admin-only');
-      else $('.admin').show();
-      next();
+        .catch(() =>page('/'));
+    
+      adminView.verify = function(ctx, next) {
+        if(!localStorage.submittedName) $('.admin').addClass('admin-only');
+        else $('.admin').show();
+        next();
+      }
     }
-  }
+    )}
   module.adminView = adminView;
-})(app)
+})(app);
