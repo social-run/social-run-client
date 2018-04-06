c87ec749ddaed6414a66d40adebeae9b94f26bb4
LACY HOGAN <lacy.hogan@gmail.com>
2018-04-06 10:39:57 -0700

    Fixing routing

--- scripts/controllers/routes.js
+++ scripts/controllers/routes.js
@@ -1,11 +1,11 @@
 'use strict'
 
-page('/', ctx => app.Run.fetchAll(app.runView.initIndexPage));
+page('/social-run-client/', ctx => app.Run.fetchAll(app.runView.initIndexPage));
 
-page('/runs/:run_id', ctx => app.Run.fetchOne(ctx, app.runView.initDetailPage));
+page('/social-run-client/runs/:run_id', ctx => app.Run.fetchOne(ctx, app.runView.initDetailPage));
 
-page('/admin', ctx => app.adminView.initAdminPage(ctx));
+page('/social-run-client/admin', ctx => app.adminView.initAdminPage(ctx));
 
-page('/about', () => app.aboutView.initAboutPage());
+page('/social-run-client/about', () => app.aboutView.initAboutPage());
 
 page();
